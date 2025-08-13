4bdab79d5db3f6cecd6bd6355c5b4e1d4e065b9f
Bhanu-singh9 <singhbhanu1922@gmail.com>
2025-08-13 13:37:47 +0545

    Creaeted[Slicing.py]

---
+++ Python/Escape.py
@@ -0,0 +1,5 @@
+'''Escape Characters'''
+z="nginx\nCopyEdit\nHello\nWorld"
+print(z)
+y='makefile\nCopyEdit\nName:John\nAge:25\n'
+print(y)
---
+++ Python/Searching.py
@@ -0,0 +1,9 @@
+'''Searching'''
+p="I love Python Programming"
+print(p)
+print(p.find("Python"))
+
+'''index'''
+q="lets code in python"
+print(q)
+print(q.index("code"))
