e4614a57c73719d9ba0bbf11794a8c4ac31cea5a
Bhanu-singh9 <singhbhanu1922@gmail.com>
2025-08-13 13:24:31 +0545

    Created[Slicing.py]

---
+++ Python/Slicing.py
@@ -0,0 +1,14 @@
+'''extract'''
+text='Python'
+print(text)
+print(text[2:6])
+
+'''extract2'''
+p="I love Programming"
+print(p)
+print(p[2:6])
+
+'''slicing'''
+word="python"
+print(word)
+print(word[::-1])
