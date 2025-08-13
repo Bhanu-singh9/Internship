18a38e839e967b44cd4c5df38c785d54957a49a4
Bhanu-singh9 <singhbhanu1922@gmail.com>
2025-08-13 12:45:56 +0545

    Add Basics.py

---
+++ Python/Basics.py
@@ -0,0 +1,22 @@
+'''Warmup-Basics
+(Length and Indexing)
+Store your quote in variable and print its length 
+print the first and last characters'''
+text="Yishan"
+print(text)
+print(len(text))
+print(text[0])
+print(text[-1])
+
+'''concatenation'''
+a='Bhanu'
+b='Singh'
+print(a+b)
+
+'''Repitition'''
+a="Python!"
+print(a*10)
+
+
+
+
