0e0e5d17392de818969748048e991afbf75b6413
Bhanu-singh9 <singhbhanu1922@gmail.com>
2025-08-13 13:10:17 +0545

    Created [Strings.py]

---
+++ Python/Strings.py
@@ -0,0 +1,21 @@
+'''String method practice'''
+a="learning python"
+print(a.upper())
+print(a.lower())
+print(a.title())
+
+'''removespaces   '''
+b=" clean me "
+print(b)
+print(b.strip())
+
+'''replace'''
+c="i have a dog"
+d="cat"
+print(c)
+print(c.replace("dog", d))
+
+'''count'''
+e="banana"
+print(e)
+print(e.count("a"))
